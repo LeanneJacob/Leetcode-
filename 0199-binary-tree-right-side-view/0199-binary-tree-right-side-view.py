5fd8be5585e46e179fab1b5e5a4ffaf0bc942c2c
LeanneJacob <leanne.serahjacob@gmail.com>
2026-09-10 18:19:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (70.36%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -11,17 +11,18 @@ class Solution:
         queue=deque([root])
         if not root:
             return []
-
         while queue:
             size=len(queue)
             for i in range(len(queue)):
-                node=queue.popleft()
-                if i== size-1:
+                node= queue.popleft()
+                if i == size-1:
                     result.append(node.val)
+
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
         return result
+
 
         
